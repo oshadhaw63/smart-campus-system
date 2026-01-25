f0083a8bf7e1a32e9728834656a1b79f8de6993c
oshadhaw63 <oshadhawijayarathne@yahoo.com>
2026-01-25 16:12:09 +0530

    fix CORS to allow Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -67,10 +67,10 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # CORS
-origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+origins = ["http://localhost:5173", "http://127.0.0.1:5173","*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
